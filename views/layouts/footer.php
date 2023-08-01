766d4b30de61407c932397fa29bf2415c2b6e092
kevin2sant <kevin770540@outlook.com>
2023-08-01 01:31:29 -0400

    delete archivos

--- views/layouts/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="row bg-light py-4 mt-auto">
-    <div class="col"> Footer content here... </div>
-</footer>
--- views/layouts/header.php
+++
@@ -1,5 +0,0 @@
-<header class="header" id="header">
-        <a class="btn open-menu" href="#" role="button" style="background-color: #92dde4;color: #fff;">
-                <i class="fas fa-align-left"></i>
-        </a>
-</header>
